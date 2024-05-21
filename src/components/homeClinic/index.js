f5d079d00a82a8bbbc1fd289db095a0183bc77c8
majolivero <majolivero28@gmail.com>
2024-05-21 14:13:29 -0500

    Actualización de perfil clínica

--- src/components/homeClinic/index.js
+++ src/components/homeClinic/index.js
@@ -1,18 +1,43 @@
+//Header y footer
+
+let icon=document.querySelector(".material-symbols-outlined");
+let menuHidden=document.querySelector(".menuHidden");
+let searchHidden=document.querySelector(".searchHidden");
+
+
+icon.addEventListener("click",(e)=>{
+    menuHidden.classList.toggle("viewMenu");
+    searchHidden.classList.remove("viewSearch");
+});
+
+let search=document.querySelector(".material-symbols-outlined1");
+
+search.addEventListener("click",(e)=>{
+    menuHidden.classList.remove("viewMenu");
+    searchHidden.classList.toggle("viewSearch");
+});
+
+
 //Section 1
 // OPEN SUBMENU
 //Add a click event to the browser window that triggers a function with the window information in the "e" parameter. 
 window.addEventListener('click', function(e){   
 //If the click target is menuIcon then enter the if statement.
     if (document.getElementById('menuIcon').contains(e.target)){
+        document.getElementById("gridSubMenu").style.display="grid";
         //Get the width of gridSubMenu and if is equal to 8 then enter the if statement.
         if (document.getElementById("gridSubMenu").offsetWidth == 8){
-                //Change the value of the width in gridSubmenu to 110%.
-                document.getElementById("gridSubMenu").style.width = "110%";
+                //Change the value of the width in gridSubmenu to 115%.            
+                document.getElementById("gridSubMenu").style.width = "115%";
             }
             //If the width the gridSubmenu is different than 8 enter the else statement.
             else{
                 //Change the value of the width in gridSubmenu for 1%.
                 document.getElementById("gridSubMenu").style.width = "1%";
+                setTimeout(() => {
+                    document.getElementById("gridSubMenu").style.display="none";
+                }, 2000);
+                
             } 
     //If the click target is not menuIcon the enter the else statement.    
     }
@@ -20,8 +45,12 @@ window.addEventListener('click', function(e){
     else if (!document.getElementById('gridSubMenu').contains(e.target)){
         //Change the value of the width in gridSubmenu for 1%.
         document.getElementById("gridSubMenu").style.width = "1%";
+        setTimeout(() => {
+            document.getElementById("gridSubMenu").style.display="none";
+        }, 2000);
     }
   });
+
 
 
 
