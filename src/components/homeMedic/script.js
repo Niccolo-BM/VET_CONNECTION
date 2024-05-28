255eed98600ca73be251276b67cbb0c17245ed6e
Niccolo-BM <nicolasbernal0821@gmail.com>
2024-05-28 16:18:11 -0500

    ok

--- src/components/homeMedic/script.js
+++ src/components/homeMedic/script.js
@@ -1,3 +1,30 @@
+// page header and footer script
+let icon=document.querySelector(".material-symbols-outlined");
+let menuHidden=document.querySelector(".menuHidden");
+let searchHidden=document.querySelector(".searchHidden");
+let body=document.querySelector(".body");
+
+icon.addEventListener("click",(e)=>{
+    menuHidden.classList.toggle("viewMenu");
+    searchHidden.classList.remove("viewSearch");
+});
+
+let search=document.querySelector(".material-symbols-outlined1");
+
+search.addEventListener("click",(e)=>{
+    menuHidden.classList.remove("viewMenu");
+    searchHidden.classList.toggle("viewSearch");
+});
+
+body.addEventListener("click",(e)=>{
+    let evento=e.target;
+    if(!evento.className.includes("material-symbols-outlined") && !evento.className.includes("material-symbols-outlined1") ){
+        menuHidden.classList.remove("viewMenu");
+        searchHidden.classList.remove("viewSearch");
+    }
+
+});
+
 import{
     getUrlParams
 }
